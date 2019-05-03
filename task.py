1cb56d498c3cbdab457a7eed235f11bfc19d75ba
theCydonian <31706764+theCydonian@users.noreply.github.com>
2019-05-03 15:08:04 -0700

    finished

--- task.py
+++ task.py
@@ -28,7 +28,9 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 1.0-0.3*(np.square(self.sim.pose[:3] - self.target_pos)).sum()
+        if abs(self.sim.pose[:3] - self.target_pos).sum() < 0.3:
+            reward += 5000/abs(self.sim.pose[:3] - self.target_pos).sum()
         return reward
 
     def step(self, rotor_speeds):
